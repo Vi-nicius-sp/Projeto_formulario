27dd51c7687843c1376994f2cc8da312f2ea582d
unknown <lab3124@etecjk.local>
2026-04-14 22:16:50 -0300

    Correção simples.

--- src/app/first-component/first-component.ts
+++ src/app/first-component/first-component.ts
@@ -19,7 +19,7 @@ formulario = new FormGroup({
 nome: new FormControl('', [Validators.required,Validators.minLength(3)]),
 idade: new FormControl(null,[Validators.required,Validators.min(0),Validators.max(120)]),
 cidade: new FormControl('',[Validators.required,Validators.minLength(3)] ),
-sexo: new FormControl('Masculino',[Validators.required])
+sexo: new FormControl('M',[Validators.required])
 
 })
 
